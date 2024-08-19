97ccd229a8c3c7f588689c4b914cad6f4d88dbd5
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-08-19 18:08:42 -0300

    [feat]scroll to contact

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import '../styles/Home.scss';
 import iDog from '../../public/iDog.png';
 export default function Home() {
+    const scrollToContact = (event) => {
+        event.preventDefault();
+        document.getElementById('footer').scrollIntoView({behavior: 'smooth'});
+    }
     return(
         <section className="home" id="home">
             <img src={iDog} alt='Home'/>
@@ -8,7 +12,7 @@ export default function Home() {
                 <h1>Hello, i'm Rafael! 👋</h1>
                 <div className='links'>
                     <a href="#about">Download CV</a>
-                    <a href="#projects">Contacts</a>
+                    <a href="#contact" onClick={scrollToContact}>Contact</a>
                 </div>
             </div>
         </section>

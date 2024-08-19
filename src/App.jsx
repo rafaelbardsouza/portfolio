46f2d7a60cf28e1993f156f8118c737247b96b03
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-08-19 18:31:08 -0300

    [feat] lang initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useEffect } from 'react';
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
+  useEffect(() => {
+    if (localStorage.getItem('lang') === null) {
+      localStorage.setItem('lang', 'english');
+    }
+  }, []);
+
   return (
     <>
-    <Home/>
-    <About/>
-    <Footer/>
+      <Home />
+      <About />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

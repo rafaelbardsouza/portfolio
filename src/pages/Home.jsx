8b25e7795dd5535ecea06a187d7c1301affee830
rafaelbardsouza <152352367+rafaelbardsouza@users.noreply.github.com>
2024-08-19 16:46:02 -0300

    [style] home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import '../styles/Home.scss';
-
+import iDog from '../assets/idog.png';
 export default function Home() {
     return(
         <section className="home" id="home">
-            <img src='https://media.giphy.com/media/3o7TKz9bX9v6hZ8lIc/giphy.gif' alt='Home'/>
+            <img src={iDog} alt='Home'/>
             <h1>Hello, i'm Rafael! 👋</h1>
         </section>
     )
